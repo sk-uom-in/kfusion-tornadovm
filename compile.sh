c4d6fe1bd2fae8b610074a9e79bdfc4c44607d74
Sambbhav khare <84086679+sk-uom-in@users.noreply.github.com>
2024-08-20 19:00:34 +0100

    wip

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 export KFUSION_ROOT="${PWD}"
 export PATH="${PATH}:${KFUSION_ROOT}/bin"
+export PATH_TO_FX=/Users/shivomkhare/Downloads/javafx-sdk-21.0-2.4/lib
+export PATH="${PATH}:${PATH_TO_FX}"
 mvn clean install -DskipTests
 
--- setvars.sh
+++ setvars.sh
@@ -4,3 +4,5 @@ export PATH="${PATH}:${KFUSION_ROOT}/bin"
 export TORNADO_ROOT=/Users/shivomkhare/UomSam/TornadoVM 
 export PATH="${PATH}:${TORNADO_ROOT}/bin/bin"
 export TORNADO_SDK=${TORNADO_ROOT}/bin/sdk
+export PATH_TO_FX=/Users/shivomkhare/Downloads/javafx-sdk-21.0-2.4/lib
+export PATH="${PATH}:${PATH_TO_FX}"
